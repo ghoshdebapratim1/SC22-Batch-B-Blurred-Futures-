b55b26b165c40139f9a55262909fa90a607b3754
ghoshdebapratim1 <dg19@illinois.edu>
2022-07-05 17:34:57 +0000

    Commit 5th July

---
+++ Week1/Intro-to-python-2022.py
@@ -0,0 +1,8 @@
+# create a dictionary with key value pairs. I'm just going to use some words and defintions 
+my_dictionary = {"dog": "a domesticated carnivorous mammal that typically has a long snout",
+                 "cat": "a small domesticated carnivorous mammal with soft fur",
+                 "bunny": "a rabbit, especially a young one.",
+                 "timberwolves": "wolves of a large variety found mainly in northern North America, with gray brindled fur."
+                 }
+
+print(my_dictionary["dog"])
---
+++ Week1/Nathan/python-test.py
@@ -0,0 +1,8 @@
+#Variables work by assigning a value to a specific word or string of characters. It can be done using the "=" sign. This assigns whatever is on the right the string of characters on the left. We'll go ahead and try assigning a few variables in the next block.
+# Raandom Text                                    pri
+a = 1
+b = 2
+c = 3
+a # when you put a variable by itself, its value will be printed (This is a comment - anything after a # on the same line will be ignor
+
+print("hello world")
